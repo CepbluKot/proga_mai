00f0c2cca3c6c60dfafd2abad82fa1781926a523
turba <55404866+CepbluKot@users.noreply.github.com>
2021-02-22 14:26:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main(int argc, char** argv) {
+int main() {
 	
 	int num = 0;
+	int num_copy = 0;
 	int step = 0;
+	int size = 0;
 	cin >> num ; 
-	cout << num % 10 << " * 10" <<" ^ "<< step ;
-	num = num / 10;
+	num_copy = num;
+	
+	int m = 0;
+	while(num)
+	{
+    	m = m*10+num%10;
+   		num /= 10;
+	}
+	
+	
+	cout << m % 10 << " * 10" <<" ^ "<< step ;
+	
+	m = m / 10;
 	step++;
-	while ( num != 0){
-		cout << " + "<< num % 10 << " * 10" <<" ^ "<< step ;
+	
+	
+	
+	
+	
+	
+	while ( m != 0){
+		cout << " + "<< m % 10 << " * 10" <<" ^ "<< step ;
 		
 	
 		step++;
 	
-		num = num / 10;
+		m = m / 10;
 		
 		
 	}
